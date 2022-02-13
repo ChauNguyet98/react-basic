e616d35d9bcfb83e5deba9073bbabb4777f2458e
ChauNguyet98 <chaunguyet1998@gmail.com>
2022-02-13 22:10:34 +0700

    state - controlling reactjs app state

--- src/views/example/my-component.js
+++ src/views/example/my-component.js
@@ -1,11 +1,41 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+
+    stage = {
+        name: 'Lalisa',
+        channel: 'Selina Vlog',
+    };
+
+    /**
+     * JSX => return a block (a tag in the outermost)
+     * fragment
+     * <React.Fragment></React.Fragment> is the same <></>
+     */
+
+    handleOnChangeName = (event) => {
+        this.setState({
+            name: event.target.value
+        });
+    }
+
     render() {
+        let name = 'Selina';
+
         return (
-            <div>
-                Hello my component!!!
-            </div>
+            <>
+                <div className="first">
+                    {console.log('My name is ', name)}
+                    <input value={this.stage.name} type="text"
+                        onChange={(event) => this.handleOnChangeName(event)}
+                    />
+                    My name is {this.stage['name']}
+                </div>
+
+                <div className="second">
+                    My youtube channel: {this.stage.channel}
+                </div>
+            </>
         );
     }
 }
