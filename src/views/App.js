8648fc4f4cb1f46b4fd142dde4e3c9b9d4226ffa
ChauNguyet98 <chaunguyet1998@gmail.com>
2022-02-19 16:57:07 +0700

    form in reactjs

--- src/views/App.js
+++ src/views/App.js
@@ -16,14 +16,6 @@ function App() {
         <p>
           Hello World with ReactJS.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
 
         <MyComponent />
       </header>
